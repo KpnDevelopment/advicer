6d5cc27ba21a7e16f9dce52e43aec8a2f812185a
Vishnu V <vishnukpn884@gmail.com>
2021-05-26 18:12:54 +0530

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState } from "react";
+import { Button, Card, Container } from "react-bootstrap";
+
+import "./App.css";
 
 function App() {
+  const [advice, setAdvice] = useState("Click the button");
+
+  const fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        console.log(response.data.slip.advice);
+        setAdvice(response.data.slip.advice);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card
+        className="card"
+        style={{
+          width: "50rem",
+          alignItems: "center",
+          padding: "3rem",
+        }}
+      >
+        <h1>" {advice} "</h1>
+        <Button style={{ width: "10rem", margin: "1rem" }} onClick={fetchAdvice}>
+          Get Advice
+        </Button>
+      </Card>
     </div>
   );
 }
